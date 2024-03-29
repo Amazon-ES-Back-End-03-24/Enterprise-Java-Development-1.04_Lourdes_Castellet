91711c1bd6c51da14866267f9db875ae56b5892c
Lourdes Castellet <castelletlourdes@gmail.com>
2024-03-29 18:23:35 +0100

    Final version with Task 2 and 3 completed

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         task1(args);
+        System.out.println("\n");
         task2(args);
+        System.out.println("\n");
         task3(args);
     }
     public static void task1(String[] args) {
         System.out.println("TASK 1");
-        int[] numbers = {1, 5, 3, 8, 13, 2};
+        int[] numbers = {1, -5, 3, -8, 13, 2};
         System.out.println("My numbers are: ");
         System.out.println(Arrays.toString(numbers));
         Arrays.sort(numbers);
@@ -22,17 +24,22 @@ public class Main {
 
     public static void task2(String[] args) {
         System.out.println("TASK 2");
-        int numbers = 1;
-        System.out.println(numbers);
-
+        int[] numbers = {10, -6, 2, 9, -13, 4};
+        System.out.println("My numbers are: ");
+        System.out.println(Arrays.toString(numbers));
+        Arrays.sort(numbers);
+        System.out.println("The 2 smallest numbers are: ");
+        for (int i = 0; i < 2; i++){
+            System.out.println(numbers[i]);
+        }
     }
 
     public static void task3(String[] args) {
         System.out.println("TASK 3");
-        int numbers = 2;
-        System.out.println(numbers);
-
+        int x = 5;
+        int y = 12;
+        System.out.println("x value is " + x + " and y value is " + y);
+        int result = x * x + ((4 * y / 5) - x) * ((4 * y / 5) - x);
+        System.out.println("Result: " + result);
     }
-
-
 }
